ce756e4cc11b4c26cd5a22e4e955c56e52dee5d4
olegrog <olegrog@5eb7ec0f-e138-4291-89eb-958b4dcaadb4>
2013-01-12 20:05:29 +0000

    script for refine stationary value
    git-svn-id: https://localhost/svn/rogsolv/trunk@7 5eb7ec0f-e138-4291-89eb-958b4dcaadb4

---
+++ tools/paraview/extrapolate.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+usage() {
+	echo "This script extrapolates investigating value along time using gnuplot"
+	echo "Usage: `$basename $0` <script> <path>"
+}
+
+[[ $# -ne 2 ]] && { echo "Illegal amount of arguments."; usage; exit 1; }
+script=$1
+path=$2
+[[ -f $script ]] || { echo "Bad script filename."; usage; exit 1; }
+[[ -d $path ]] || { echo "Bad dirname."; usage; exit 1; }
+
+tmp=$(mktemp)
+for dir in $(find $path -name VTK); do
+	echo $dir
+	: > $tmp
+	for f in $(ls $dir | grep vtk | sort -n); do
+		$(dirname $script)/$(basename $script) $dir/$f | tail -1 | awk '{print $3}' >> $tmp		# write to tmp file investigated values
+	done
+	sed -i '1,5d' $tmp							# remove first 5 values
+	num=$(wc -l $tmp | awk '{print $1}')
+	echo "$num files were used"
+	[[ $num -eq 0 ]] && continue
+	# evaluate mean value
+	cat $tmp | python -c 'from sys import stdin; import numpy as np; \
+	       	a = [float(i) for i in stdin.read().split()]; print "mean = %.4f" % np.mean(a); print "sigma = %.2e" % np.std(a)'
+done
